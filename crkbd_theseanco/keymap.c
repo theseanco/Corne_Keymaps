31a4b5ad451e5cafd5fe8e491aa1a7db70dd7eed
theseanco <theseancotterill@live.com>
2020-01-05 19:28:51 +0000

    added layer 1

--- crkbd_theseanco/keymap.c
+++ crkbd_theseanco/keymap.c
@@ -33,13 +33,13 @@ enum macro_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
+       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,                         KC_NO,    KC_Y,    KC_U,    KC_I,   KC_O,  KC_P,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
+      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,                         KC_NO,    KC_H,    KC_J,    KC_K,   KC_L,  KC_SCLN,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
+      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TAB,                   LGUI(KC_ESC),  KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,  RAISE,  KC_RALT \
+                       LALT_T(KC_HOME),LGUI_T(KC_ENT),LCTL_T(KC_ESC)  ,LSFT_T(KC_BSPC),LT(1,KC_SPC),LT(2,KC_END)\
                                       //`--------------------------'  `--------------------------'
 
   ),
