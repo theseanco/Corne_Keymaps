ecf8a5f19515b7166240dfe9ea367266f91ab7f9
theseanco <theseancotterill@live.com>
2020-01-05 19:36:32 +0000

    added layer 2

--- crkbd_theseanco/keymap.c
+++ crkbd_theseanco/keymap.c
@@ -46,13 +46,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,\
+       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,                        TO(3),   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,   \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,\
+      KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,                       KC_NO,  KC_LEFT,  KC_DOWN, KC_UP, KC_RIGHT,  KC_PGUP,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      KC_F6,   KC_F7,    KC_F8, KC_F9,    KC_F10,   KC_NO,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_PGDN,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RALT \
+                                          KC_NO,   KC_NO,  KC_NO,     KC_NO,   KC_NO, KC_NO \
                                       //`--------------------------'  `--------------------------'
     ),
 
